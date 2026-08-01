d85319f4800bce09ed049ae1b2870f946675eaea
Puneeth Prabhakar <babloopuneeth@gmail.com>
2026-08-01 22:58:04 +0530

    Fixed favicon

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className={`${outfit.variable} ${inter.variable}`}>
+		<html lang="en" className={`${outfit.variable} ${inter.variable}`} data-scroll-behavior="smooth">
 			<body>
 				<TRPCProvider>{children}</TRPCProvider>
 			</body>
